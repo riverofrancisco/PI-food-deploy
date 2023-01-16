a90034c432fd85a7bd7ee16516fe838b82914d1a
riverofrancisco <francisco.rivero124@gmail.com>
2023-01-16 15:19:11 -0300

    Author Added

---
+++ client/src/Components/AuthorLink.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './AuthorLink.css';
+
+export default function Author(){
+    
+    return (
+        <div >
+            <a href="https://www.linkedin.com/in/rivero-francisco/">
+                <img className="logoAuthor" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt='in' /> 
+            </a>
+        </div>
+    );
+}
--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -80,7 +80,7 @@ export default function Navbar({setOrder, paginate}){
         </nav>
         
         <div className='authorLink'>
-             <Author/>
+             <Author />
         </div>
 
         <div className='dietSelected'>
