4c539ef33b6ee07c7aa91807a4744c868cc41ab9
riverofrancisco <francisco.rivero124@gmail.com>
2023-06-16 20:43:47 -0300

    Updates deployed

--- client/src/Components/AuthorLink.jsx
+++ client/src/Components/AuthorLink.jsx
@@ -8,6 +8,9 @@ export default function Author(){
             <a href="https://www.linkedin.com/in/rivero-francisco/">
                 <img className="logoAuthor" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt='in' /> 
             </a>
+            <a href="https://franciscorivero-portfolio.vercel.app/">
+                <img className="logoAuthor" src="https://franciscorivero-portfolio.vercel.app/static/media/LOGO.9ac52e9611515aaa605f.png" alt='in' /> 
+            </a>
         </div>
     );
 }
--- client/src/Components/Welcome.jsx
+++ client/src/Components/Welcome.jsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 import './Welcome.css';
 
 export default function Welcomepage(){
-    
+  
     return (
         <div className='container'>
 
             <div className="content">
+            {alert("This is not a Responsive App")}
             
             <h1 className='WelcomeTitle'>Food API</h1>
 
